86c8789d67c7783f1dc886340f71856c52c7a55d
ryu <uniquefox929304@gmail.com>
2025-11-11 12:23:09 -0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import ProfileCard from '../components/ProfileCard'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <ProfileCard />
+    </div>
+  )
+}
