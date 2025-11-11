4f472545fc1f434ce98ba2291309419f6144646e
ryu <uniquefox929304@gmail.com>
2025-11-11 12:25:47 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images-ext-1.discordapp.net', 'cdn.discordapp.com'],
+  },
+}
+
+module.exports = nextConfig
